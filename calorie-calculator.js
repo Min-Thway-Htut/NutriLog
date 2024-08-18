fdae7701cb58aac19939626f632434bd710ce510
Min-Thway-Htut <minthwayhtut568@gmail.com>
2024-08-18 03:50:38 +0200

    Deleting unnecessary files

--- calorie-calculator.js
+++ calorie-calculator.js
@@ -29,3 +29,44 @@ function calculateCalorie() {
     // Display the result
     document.getElementById('calorie-result').innerHTML = "You require " + bmr.toFixed() + " calories per day!";
 }
+
+function calculateBMI(){
+
+    let bmiHeight = parseFloat(document.getElementById('bmi-height').value);
+    let bmiWeight = parseFloat(document.getElementById('bmi-weight').value);
+    let userValidAge = "";
+    const ageRadios = document.getElementsByName("valid-age");
+
+    for(let i = 0; i < ageRadios.length; i++){
+        if (ageRadios[i].checked){
+            userValidAge = ageRadios[i].value;
+        }
+    }
+
+    let bmi = 0;
+
+    if (userValidAge === "yes"){
+        bmi = (bmiWeight / (bmiHeight * bmiHeight)) * 10000; // Calculate BMI
+        let finalBmi = bmi.toFixed(1);
+
+        if(finalBmi <= 18.5){
+          document.getElementById('bmi-result').innerHTML = "BMI - " + finalBmi + " You are underweight!";
+        }else if(finalBmi >= 18.6 && finalBmi <= 24.9){
+          document.getElementById('bmi-result').innerHTML = "BMI - " + finalBmi + " Don't worry, it is normal!";
+        }else if(finalBmi >= 25.0 && finalBmi <= 29.9){
+          document.getElementById('bmi-result').innerHTML = "BMI - " + finalBmi + " You are overweight!";
+        }else{
+          document.getElementById('bmi-result').innerHTML = "BMI - " + finalBmi + " It is obesity!";
+        }
+    } else if (userValidAge === "no"){
+        document.getElementById('bmi-result').innerHTML = "Sorry, we cannot assure the correct result based on your age."
+    } else{
+        document.getElementById('bmi-result').innerHTML = "Please select your gender!";
+    }
+
+ 
+
+    // formula - [weight (kg) / height (cm) / height (cm)] x 10,000
+    // document.getElementById('bmi-result').innerHTML = bmiHeight + " " +bmiWeight +" " + bmiAge + " " + userValidAge + bmi.toFixed();
+
+}
